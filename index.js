cb0766149cdd618a54919da8d0cc643fe604fe5f
Ifranhugo <ifranluzbelito@gmail.com>
2021-12-02 18:59:16 -0300

    seguir import parametrs

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 import responiveMedia from "./indexResponsev.js";
-let contPubli = `<button class="carrusel_izquierdo" id="carrusel_izquierdoID"><i class="fas fa-chevron-right"></i></button>
-      <section class="cont-publiJS-1">
-        <div class="bu-publicidadJS">
-        <img src="" alt="">
-          <a href="#" class="bu-publi">ver</a>
-        </div>
+let contPubli = `   
+       <button class="carrusel_adelante"  value="" id="carrusel_izquierdoID"><i class="fas fa-chevron-left">
+      </i></button>
+      <button class="carrusel_atras"  value=""  id="carrusel_izquierdoID"><i class="fas fa-chevron-right"></i></button>
+    
+      <img src="", frj)" alt=""  id="imgPubli">`;
 
-      </section>
-      <section class="cont-publiJS-2">
-        <div class="bu-publicidad">
-          <a href="accesorios.html" class="bu-publi">ver</a>
-        </div>
-
-      </section>
-      <section class="cont-publiJS-3">
-        <div class="bu-publicidad">
-        
-          <a href="accesorios.html" class="bu-publi">ver</a>
-        </div>
-      </section>
-      <section class="cont-publiJS-4">
-        <div class="bu-publicidad">
-          <a href="accesorios.html" class="bu-publi">ver</a>
-        </div>
-      </section>
-      <button class="carrusel_izquierdo" id="carrusel_izquierdoID"><i class="fas fa-chevron-left">
-      </i></button>`;
-
-console.log("hola");
 let body__practicaID = document.getElementById("body__practicaID");
 let containPrincipalID = document.getElementById("containPrincipalID");
 responiveMedia(
   "containPrincipalID",
-  "(min-width: 330px)",
-  `${contPubli} `,
+  "(max-width: 330px)",
+  `${contPubli}`,
   "desktopcontent"
 );
+import { Carrusel, request, frj } from "./indexResponsev.js";
+request(frj, log);
+let flechaatras = contenedor.querySelector(".carrusel_atras");
+export { flechaatras };
+document.addEventListener("domCont", () => {
+  request(frj, Carrusel);
+
+  console.log(contenedor, "euuuu");
+});
+
+function log(content) {
+  console.log(content);
+}
--- indexResponsev.js
+++ indexResponsev.js
@@ -10,20 +10,56 @@ export default function responiveMedia(id, mq, movilecontent, desktopcontent) {
     } else {
       d.getElementById(id);
     }
-    console.log(e.matches, breackpoint);
+    Carrusel(containPrincipalID, frj);
   };
+
   breackpoint.addListener(resposive);
   resposive(breackpoint);
-  function log(content) {
-    console.log(content);
-  }
-
-  function print(params) {}
-  async function request(frj, fn) {
-    let respuesta = await fetch(frj);
-    let json = await respuesta.json();
-    fn(json);
-  }
-  request(frj, log);
 }
-let frj = "./imag_publi.json";
+document.addEventListener("domCont", () => {
+  request(frj, Carrusel(containPrincipalID));
+  console.log(contenedor, "euuuu");
+});
+
+function log(content) {
+  console.log(content);
+}
+import { flechaatras } from "./index.js";
+export let frj = "./imag_publi.json";
+export async function request(frj, fun) {
+  let respuesta = await fetch(frj);
+  let json = await respuesta.json();
+  let jsonArray = Object.entries(json);
+  fun(containPrincipalID, jsonArray, flechaatras);
+}
+
+request(frj, log);
+export function Carrusel(contenedor, nam, atras) {
+  contenedor.addEventListener("click", (ed) => {
+    (adelante = contenedor.querySelector(".carrusel_adelante")),
+      (cont = 0),
+      (imgPubli = contenedor.querySelector("imgPubli")),
+      (tgt = ed.target),
+      (array = [nam]);
+    array.forEach((ent) => {
+      if (tgt === atras) {
+        console.log("anda");
+        if (cont > 0) {
+          imgPubli.src = ent[cont - (1)[1]];
+          cont--;
+        } else {
+          imgPubli.src = ent[ent.length - 1];
+          cont = ent.length - 1;
+        }
+      } else if (tgt == adelante) {
+        if (cont > ent.length - 1) {
+          imgPubli.src = ent[ent.length + 1];
+          cont++;
+        } else {
+          imgPubli.src = ent[0][1];
+          cont = 0;
+        }
+      }
+    });
+  });
+}
