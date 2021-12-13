5734eaae682dc21ee8f42c403907aef6d06a8d07
ifranHugo <95197883+ifranHugo@users.noreply.github.com>
2021-12-13 01:56:43 +0000

    Deploying to gh-page from @ ifranHugo/nuevorepositorio@56b4a5f04c26b04d2e2891cd2eceb29cf73d9009 🚀

--- producto.js
+++ producto.js
@@ -13,5 +13,3 @@ let button_carrrID = document.getElementById("button_carrrID");
 button_carrrID.addEventListener("click", () => {
   seccionCarID.classList.toggle("seccionCarCambio");
 });
-const $pasarMuestraId = document.querySelector(".a_fotogalery"),
-  foto_galeria_1 = document.querySelector(".foto-galeria-1");
