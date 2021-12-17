bfe885f912af31a216531e5297040180fb6c7daf
ifranHugo <95197883+ifranHugo@users.noreply.github.com>
2021-12-17 15:01:47 +0000

    Deploying to gh-page from @ ifranHugo/nuevorepositorio@acd68a58fdf1a7fa2f352b77aa7e4f6e97a17ad1 🚀

--- menuJS.js
+++ menuJS.js
@@ -6,13 +6,8 @@ var sectionMenu = `        <section class="menu">
           <li><a href="novedades.html"> <span class="off">NOVEDADES</span></a></li>
           <li><a href="productos.html" class="productos"><span class="off"> PRODUCTOS</span></a></li>
           <li class="off-1"><a href="accesorios.html"><span class="off-1"> ACCESORIOS</span></a></li>
-          <li><button class="button_carrr" id="button_carrrID"><i class="fas fa-shopping-bag "></i></li></button>
-          <li><svg class="lupa" width="25" height="21" viewBox="0 0 65 61" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M25.4061 0.5C11.7233 0.5 0.628906 10.9418 0.628906 23.8198C0.628906 36.6978 11.7233 47.1395 25.4062 47.1395C30.6678 47.1395 35.5474 45.5949 39.5584 42.9639L39.5613 42.9611L57.9372 60.2562C58.2782 60.5771 58.8205 60.5854 59.1703 60.2562L64.1229 55.595C64.4639 55.274 64.4464 54.7418 64.12 54.4345L45.7467 37.1422C48.5422 33.3644 50.1834 28.7717 50.1834 23.8198C50.1834 10.9418 39.089 0.5 25.4061 0.5ZM25.4061 41.6525C14.9413 41.6525 6.45877 33.6689 6.45877 23.8198C6.45877 13.9706 14.9414 5.98702 25.4061 5.98702C35.8708 5.98702 44.3534 13.9706 44.3534 23.8198C44.3534 33.6689 35.8709 41.6525 25.4061 41.6525Z"
-                fill="black" />
-            </svg></li>
+          <li><button class="button_carrr" id="button_carrrID"><i class="fas fa-shopping-bag "></i></button></li>
+          <li><button class="button_busc"><i class="fas fa-search"></i></button></li>
         </ul>
       </section>
     </section>`,
@@ -185,7 +180,7 @@ export function CargarProduct() {
         datos.forEach((producto) => {
           insertProduct.innerHTML += `
     <div class="foto_product  ${producto.id}" id="foto_productID">
-          <img class="foto-galeria-1 a_fotogalery" src="./${producto.img1}" alt="">¿
+          <img class="foto-galeria-1 a_fotogalery" src="./${producto.img1}" alt="">
         <div class="carrto__comprar" id="carrto__comprarID"><button class="buttom__galery" id="buttom__galeryID">agregar
             al carrtito<i class="fas fa-shopping-cart carrLog" _mstvisible="2"></i></button></div>
         <div class="info-galeria">
